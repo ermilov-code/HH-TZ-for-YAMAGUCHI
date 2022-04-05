7aee5c5d546f4dc63cb84d8f54f04f96173c00a8
paratrooper-94 <ermilov1994@yandex.ru>
2022-04-05 14:32:18 +0300

    - [x] HTML markup of the table section - [x] Desktop version of the table - Grid - [x] HTML markup for the section-voice-control section - [ ] Animation on scroll
    - [x] HTML разметка секции table
    - [x] Десктоп версия стола - Grid
    - [x] HTML разметка секции section-voice-control
    - [ ] Анимация при scroll

--- js/main.js
+++ js/main.js
@@ -7,4 +7,26 @@ let countertop = document.getElementById('countertop');
 tableBtnUp.addEventListener("click", () => countertop.classList.add('tableAnimationCSS'));
 tableBtnDown.addEventListener("click", () => countertop.classList.remove('tableAnimationCSS'));
 
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+	const scrollUpButton = document.querySelector('.woman-block');
+	const scrolledWoman = document.querySelector('.woman-block__img');
+	const scrolledEllipse = document.querySelector('.woman-block__ellipse');
+
+
+	window.addEventListener('scroll', function () {
+		// const scrolled = scrollUpButton.pageYOffset;
+		const scrolled = scrollUpButton.pageYOffset || document.documentElement.scrollTop;
+
+		if (scrolled >= 200) {
+			scrolledWoman.classList.add('animWomanUplift2');
+			scrolledEllipse.classList.add('animWomanUplift3');
+			scrollUpButton.classList.add('animWomanUplift4');
+		}
+	});
+});
+
+
 // ============================================================
