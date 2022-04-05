dbd219b86054edadab6bdb077842031ccc7e3a8d
paratrooper-94 <ermilov1994@yandex.ru>
2022-04-05 09:52:14 +0300

    - [x] Prepare and compress images - [ ] HTML markup of the table section
    - [x] Подготовка и сжатие изображений
    - [ ] HTML разметка секции table

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // ============================================================
 
+let tableBtnUp = document.getElementById('tableBtnUp');
+let tableBtnDown = document.getElementById('tableBtnDown');
+let countertop = document.getElementById('countertop');
 
+tableBtnUp.addEventListener("click", () => countertop.classList.add('tableAnimationCSS'));
+tableBtnDown.addEventListener("click", () => countertop.classList.remove('tableAnimationCSS'));
 
 // ============================================================
